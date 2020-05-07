6aaf65be441689d36ce0f9aca17fb31de6a0c2eb
Hardikgiri Goswami <hardikgiri.goswami@gmail.com>
2020-05-07 23:20:50 +0530

    [ADD]Module hspl_no_mail_server_copied.

---
+++ hspl_no_mail_server_copied/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2020 Heliconia Solutions Pvt. Ltd., < hello@heliconia.io >
+
+{
+    'name': 'No Mail Server Copied',
+    'version': '13.0.1.1.0',
+    'category': 'Base',
+    'sequence': 15,
+    'summary': 'Prevents copy of mail servers while duplication / backup',
+    'author': "Heliconia Solutions Pvt. Ltd.",
+    'description': """
+No Mail Server Copied
+==================================
+
+Prevents copy of mail servers while duplication / backup
+
+    """,
+    'website': "https://heliconia.io",
+    'depends': ['web'],
+    'license': 'OPL-1',
+    'data': [
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'images': ['static/description/hspl_no_mail_server_copied_banner.gif']
+}
