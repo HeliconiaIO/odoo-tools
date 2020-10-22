7e7cf90449987e8a243dcb3cbd0398b901a687ac
Pratik Devaliya <pratik@heliconia.in>
2020-10-22 12:13:13 +0530

    [ADD]hspl_website_payment_terms

---
+++ hspl_website_payment_terms/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018, 2020 Heliconia Solutions Pvt Ltd (https://heliconia.io)
+{
+    'name': "Website payment terms check",
+
+    'summary': """""",
+
+    'description': """
+    """,
+
+    'author': 'Heliconia Solutions Pvt. Ltd.',
+    'company': 'Heliconia Solutions Pvt. Ltd.',
+    'website': 'https://heliconia.io',
+
+
+    'category': 'Utility',
+    'version': '13.0.1.0.0',
+
+    'depends': ['website_sale'],
+
+    'data': [
+        'views/payment.xml',
+    ],
+
+    'application': False,
+    'installable': True,
+}
