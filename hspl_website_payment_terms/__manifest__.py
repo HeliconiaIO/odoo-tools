60c7b6729bae19d14593297c9b51fee490c1f936
Pratik Devaliya <pratik@heliconia.in>
2020-10-22 12:42:54 +0530

    [IMP]Manifest and license file added.

--- hspl_website_payment_terms/__manifest__.py
+++ hspl_website_payment_terms/__manifest__.py
@@ -1,27 +1,22 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018, 2020 Heliconia Solutions Pvt Ltd (https://heliconia.io)
 {
-    'name': "Website payment terms check",
-
-    'summary': """""",
-
-    'description': """
-    """,
-
-    'author': 'Heliconia Solutions Pvt. Ltd.',
-    'company': 'Heliconia Solutions Pvt. Ltd.',
-    'website': 'https://heliconia.io',
-
-
-    'category': 'Utility',
+    'name': "HSPL Website Payment Terms ",
     'version': '13.0.1.0.0',
-
+    'summary': """Check for shop/payment terms before payment""",
+    'category': 'Utility',
+    'description': """Shop Payment Terms 
+    """,
+    'author': "Heliconia Solutions Pvt. Ltd.",
+    'website': "https://heliconia.io",
+    'license': 'OPL-1',
     'depends': ['website_sale'],
-
+    'qweb': [],
     'data': [
         'views/payment.xml',
     ],
-
-    'application': False,
     'installable': True,
+    'auto_install': False,
+    'application': False,
+    'images': []
 }
