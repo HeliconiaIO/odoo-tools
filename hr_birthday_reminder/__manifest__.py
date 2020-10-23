580d0269a9297af88ec3c69bb5b461ab0279327f
th_vrushparva <vrushparva@heliconia.in>
2020-10-23 20:13:28 +0530

    [ADD]Added module hr_birthday_reminder_v13c

---
+++ hr_birthday_reminder/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Employee Birthday Reminder',
+
+    'version': '13.0.0.1',
+
+    'category': 'hr',
+    'summary': 'Birthday Reminder for Employee',
+
+    'description': """
+        This module send email notification to wish birthday
+        * Employee
+    """,
+
+    'author': "Heliconia Solutions Pvt. Ltd.",
+    'website': "https://heliconia.io/",
+
+    'depends': ['base', 'hr'],
+
+    'data': [
+        'data/birthday_reminder_cron.xml',
+        'data/mail_template_birthday_data.xml',
+        'data/ir_configparameter_view.xml',
+        'views/res_config_Setting_view.xml',
+    ],
+
+    'images': ['static/description/heliconia_employee_birthday_reminder.png'],
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ hr_birthday_reminder/models/res_config_settings.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    send_wish_employee = fields.Boolean(string='Send wish to employee')
+    emp_wish_template_id = fields.Many2one('mail.template', string='Employee Email Template')
+    send_wish_manager = fields.Boolean(string='Send wish to manager')
+    manager_wish_template_id = fields.Many2one('mail.template', string='Manager Email Template')
+    reminder_before_day = fields.Integer(string='Send reminder before days')
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        IrConfigParameter = self.env['ir.config_parameter'].sudo()
+        send_wish_employee = IrConfigParameter.get_param("employee.send_wish_employee")
+        emp_wish_template_id = IrConfigParameter.get_param("employee.emp_wish_template_id")
+        send_wish_manager = IrConfigParameter.get_param("employee.send_wish_manager")
+        manager_wish_template_id = IrConfigParameter.get_param("employee.manager_wish_template_id")
+        reminder_before_day = IrConfigParameter.get_param("employee.reminder_before_day")
+        res.update(
+            send_wish_employee=True if send_wish_employee == 'True' else False,
+            emp_wish_template_id=int(emp_wish_template_id),
+            send_wish_manager=True if send_wish_manager == 'True' else False,
+            manager_wish_template_id=int(manager_wish_template_id),
+            reminder_before_day=int(reminder_before_day),
+        )
+        return res
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].set_param("employee.send_wish_employee",
+                                                  'True' if self.send_wish_employee else 'False')
+        self.env['ir.config_parameter'].set_param("employee.emp_wish_template_id",
+                                                  self.emp_wish_template_id.id or False)
+        self.env['ir.config_parameter'].set_param("employee.send_wish_manager",
+                                                  'True' if self.send_wish_manager else 'False')
+        self.env['ir.config_parameter'].set_param("employee.manager_wish_template_id",
+                                                  self.manager_wish_template_id.id or False)
+        self.env['ir.config_parameter'].set_param("employee.reminder_before_day", self.reminder_before_day or 0)
